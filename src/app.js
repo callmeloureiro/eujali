25ba9fc86367a6aee873ed70b04e91976c477ebe
Matheus Loureiro Paz <matheus.paz@corp.nova>
2018-05-11 18:46:39 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './templates/App';
+
+const element = document.getElementById('app');
+
+ReactDOM.render(<App />, element);
---
+++ src/templates/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './App.css';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>React it´s work!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
